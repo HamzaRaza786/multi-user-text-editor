6e1ceadc6ad19f3f2e5af89d5a28f6eb92518b25
HamzaRaza786 <hamza.raza2015@outlook.com>
2024-06-10 23:33:22 +0500

    Added initial code

---
+++ client/multi-user-text-editor/src/app/editor/editor.tsx
@@ -0,0 +1,5 @@
+export default function Editor() {
+  return (
+    <div><h1>Text Editor</h1></div>
+  );
+}
---
+++ client/multi-user-text-editor/src/app/hello/page.tsx
@@ -0,0 +1,7 @@
+export default function Hello() {
+	return (
+		<div>
+			<h1>Hello how are you?</h1>
+		</div>
+	)
+}
